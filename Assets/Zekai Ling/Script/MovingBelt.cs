5411a9efabd9c115dfe30cd23ea68a4bdec4046e
CyanSmoke <2568268303@qq.com>
2025-10-05 10:23:01 +0200

    111

--- Assets/Zekai Ling/Script/MovingBelt.cs
+++ Assets/Zekai Ling/Script/MovingBelt.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MovingBelt : MonoBehaviour
 {
     public float speed = 2f;          // 传送带移动速度
+
+
+   // public float forceAmount = 5f;    // 施加的力的大小
     public Vector2 direction = Vector2.right; // 移动方向（右）
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -15,5 +18,11 @@ public class MovingBelt : MonoBehaviour
             // 沿着传送带方向施加速度
             rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
         }
+        /*  Rigidbody2D rb = collision.rigidbody;
+          if (rb != null)
+          {
+              // 持续施加一个向右的推力，而不是修改速度
+              rb.AddForce(Vector2.right * forceAmount, ForceMode2D.Force);
+          }*/
     }
 }
