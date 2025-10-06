ff8ba1140108ad8725fdb45ae6a3359338fbb54a
CyanSmoke <2568268303@qq.com>
2025-10-06 17:40:59 +0200

    num text

--- Assets/Zekai Ling/Script/SpawnObject.cs
+++ Assets/Zekai Ling/Script/SpawnObject.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
-    public Animator spawnAnimator; //生成动画控制器
+     Animator spawnAnimator; //生成动画控制器
+
+     TextMeshProUGUI remainingText; //分数文本
 
     public float spawnAnimationDuration = 0.5f; //生成动画持续时间
 
@@ -22,7 +25,13 @@ public class SpawnObject : MonoBehaviour
 
     private void Start()
     {
+        //搜寻该物体的子物体中名字为NumText的TextMeshProUGUI组件
+        remainingText = transform.Find("NumText").GetComponent<TextMeshProUGUI>();
+
+        spawnAnimator = GetComponent<Animator>();
         timer = spawnInterval; //初始化计时器
+
+        remainingText.text = (objectPrefabs.Length - index).ToString();
     }
 
     private void Update()
@@ -52,6 +61,7 @@ public class SpawnObject : MonoBehaviour
         //从0到objectPrefabs.Length-1依次间隔时间生成物体
         Instantiate(objectPrefabs[index], spawnPoint.position, Quaternion.identity);
         index++;
+        remainingText.text = (objectPrefabs.Length - index).ToString();
         //如果index达到数组长度则不再增加和生成        
     }
 }
